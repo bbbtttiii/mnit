b516228a1cd17a943d3871d1d5f25eb939c4541a
bbbtttiii <ben.iburg@gmail.com>
2021-02-06 20:41:01 -0600

    mikro-orm setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroConfig from './mikro-orm.config';
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [],
-    dbName: 'mnit',
-    type: 'postgresql',
-    debug: !__prod__
-  });
+  const orm = await MikroORM.init(mikroConfig);
+
+  //create a new post
+  const post = orm.em.create(Post, {title: 'my first post'});
+
+  //insert into db
+  await orm.em.persistAndFlush(post);
+  console.log('-------sql2-------');
 };
 
-main();
+
+
+main()
